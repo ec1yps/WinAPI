135050d1f3a883514536c94b5ca524612fe4bb1b
ec1yps_ <Mihli_sk06@top-academy.ru>
2024-10-27 13:19:24 +0300

    Add project files.

---
+++ WinAPI/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <Windows.h>
+
+//#define MESSAGE_BOX
+
+INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+{
+#ifdef MESSAGE_BOX
+	MessageBox
+	(
+		NULL,
+		"Hello WinAPI!\nЭто MessageBox, детка.",
+		NULL,
+		MB_YESNOCANCEL | MB_ICONQUESTION | MB_HELP | MB_DEFBUTTON4 | MB_SYSTEMMODAL
+	);
+#endif // MESSAGE_BOX
+
+	return 0;
+}
