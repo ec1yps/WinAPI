1f731e44c939c9e4a3d6c3bdaaf0aa3301e7b607
ec1yps_ <Mihli_sk06@top-academy.ru>
2024-10-31 18:59:48 +0300

    IDC_EDIT_LOGIN in progress

--- DialogBox/main.cpp
+++ DialogBox/main.cpp
@@ -3,6 +3,7 @@
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
+
 INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
 {
 	DialogBoxParam(hInstance, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
@@ -11,17 +12,43 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 
 BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	BOOL isHintVisible = TRUE;
+	CONST INT E_SIZE = 30;
+	CHAR edit_buffer[E_SIZE] = {};
+
 	switch (uMsg)
 	{
 	case WM_INITDIALOG:
 	{
 		HICON hIcon = LoadIcon(GetModuleHandle(NULL), MAKEINTRESOURCE(IDI_ICON1));
 		SendMessage(hwnd, WM_SETICON, 0, (LPARAM)hIcon);
+		//SetDlgItemText(hwnd, IDC_EDIT_LOGIN, "Введите имя пользователя");
 	}
 	break;
 	case WM_COMMAND:
 		switch (LOWORD(wParam))
 		{
+		case IDC_EDIT_LOGIN:
+		{
+			if (HIWORD(wParam) == EN_SETFOCUS) 
+			{
+				if (isHintVisible) 
+				{
+					SetWindowText((HWND)IDC_EDIT_LOGIN, "");
+					isHintVisible = false;
+				}
+			}
+			else if (HIWORD(wParam) == EN_KILLFOCUS) 
+			{
+				if (GetWindowTextLength((HWND)IDC_EDIT_LOGIN) == 0) 
+				{
+					SetWindowText((HWND)IDC_EDIT_LOGIN, "Введите имя пользователя");
+					GetWindowText((HWND)IDC_EDIT_LOGIN, edit_buffer, E_SIZE);
+					isHintVisible = true;
+				}
+			}
+		}
+		break;
 		case IDC_BUTTON_COPY:
 		{
 			HWND hEditLogin = GetDlgItem(hwnd, IDC_EDIT_LOGIN);
