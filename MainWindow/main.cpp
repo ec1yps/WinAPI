36755fdaedfa8d1df1b8006bda2e0652e66d387a
ec1yps_ <Mihli_sk06@top-academy.ru>
2024-11-09 19:07:29 +0300

    Homework DONE

--- MainWindow/main.cpp
+++ MainWindow/main.cpp
@@ -1,4 +1,7 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <Windows.h>
+#include <stdio.h>
+#include<iostream>
 
 CONST CHAR g_sz_MY_WINDOW_CLASS[] = "My Window";
 
@@ -40,6 +43,26 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		CW_USEDEFAULT, CW_USEDEFAULT,
 		NULL, NULL, hInstance, NULL
 	);
+
+	RECT window;
+	RECT screen;
+
+	GetWindowRect(hwnd, &window);
+	GetWindowRect(GetDesktopWindow(), &screen);
+
+	INT sizeX = (screen.right - screen.left) * 0.75;
+	INT sizeY = (screen.bottom - screen.top) * 0.75;
+
+	SetWindowPos(hwnd, NULL, 0, 0, sizeX, sizeY, SWP_NOMOVE);
+
+	GetWindowRect(hwnd, &window);
+	GetWindowRect(GetDesktopWindow(), &screen);
+
+	INT X = (screen.right - screen.left - (window.right - window.left)) / 2;
+	INT Y = (screen.bottom - screen.top - (window.bottom - window.top)) / 2;
+
+	SetWindowPos(hwnd, NULL, X, Y, 0, 0, SWP_NOSIZE);
+
 	ShowWindow(hwnd, nCmdShow);
 	UpdateWindow(hwnd);
 
@@ -49,7 +72,6 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
-
 	return msg.message;
 }
 
@@ -58,7 +80,27 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	switch (uMsg)
 	{
 	case WM_CREATE:
+		AllocConsole();
+		freopen("CONOUT$", "w", stdout);
 		break;
+	case WM_MOVE:
+	case WM_SIZE:
+	{
+		RECT wnd;
+		GetWindowRect(hwnd, &wnd);
+		printf("Location: %ix%i;\nSize:\t%ix%i;", wnd.left, wnd.top, wnd.right, wnd.bottom);
+		printf("\n-----------------------------------------\n");
+
+		INT window_width = wnd.right - wnd.left;
+		INT window_height = wnd.bottom - wnd.top;
+
+		CONST INT SIZE = 256;
+		CHAR title[SIZE]{};
+		
+		sprintf(title, "Location: %ix%i: Size: %ix%i", wnd.left, wnd.top, window_width, window_height);
+		SetWindowText(hwnd, title);
+	}
+	break;
 	case WM_COMMAND:
 		break;
 	case WM_DESTROY:
