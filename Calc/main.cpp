e51a3baa7ec88911a23ada3f217e54b3d148c83b
ec1yps_ <Mihli_sk06@top-academy.ru>
2024-11-19 21:31:30 +0300

    Calculations in progress

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -83,6 +83,12 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 
 INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
+	static DOUBLE a = DBL_MIN;
+	static DOUBLE b = DBL_MIN;
+	static INT operation = 0;
+	static BOOL input = FALSE;
+	static BOOL input_operation = FALSE;
+
 	switch (uMsg)
 	{
 	case WM_CREATE:
@@ -189,11 +195,6 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		);
 	}
 	break;
-	/*case WM_KEYDOWN:
-	{
-		if (wParam >= VK_0 && wParam <= VK_9)
-			wParam = wParam - VK_0 + IDC_BUTTON_0;
-	}*/
 	case WM_COMMAND:
 	{
 		CONST INT SIZE = 256;
@@ -204,12 +205,15 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 
 		if (LOWORD(wParam) >= IDC_BUTTON_0 && LOWORD(wParam) <= IDC_BUTTON_9)
 		{
+			if (input_operation) sz_display[0] = 0;
 			sz_digit[0] = LOWORD(wParam) - IDC_BUTTON_0 + '0';
 			if (strlen(sz_display) == 1 && sz_display[0] == '0')
 				sz_display[0] = sz_digit[0];
 			else
 				strcat(sz_display, sz_digit);
 			SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)sz_display);
+			input = TRUE;
+			input_operation = FALSE;
 		}
 
 		if (LOWORD(wParam) == IDC_BUTTON_POINT)
@@ -227,10 +231,44 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			else sz_display[strlen(sz_display) - 1] = 0;
 			SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)sz_display);
 		}
-		
+
 		if (LOWORD(wParam) == IDC_BUTTON_CLEAR)
 		{
+			a = b = DBL_MIN;
+			operation = 0;
+			input = FALSE;
+			input_operation = FALSE;
+
 			SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)"0");
+		}
+
+		if (LOWORD(wParam) >= IDC_BUTTON_PLUS && LOWORD(wParam) <= IDC_BUTTON_SLASH)
+		{
+			if (a == DBL_MIN) a = atof(sz_display);
+			//else b = atof(sz_display);
+			//input = FALSE;
+			SendMessage(hwnd, WM_COMMAND, LOWORD(IDC_BUTTON_EQUAL), 0);
+			operation = LOWORD(wParam);
+			input_operation = TRUE;
+		}
+
+		if (LOWORD(wParam) == IDC_BUTTON_EQUAL)
+		{
+			if (input) b = atof(sz_display);
+			input = FALSE;
+
+			switch (operation)
+			{
+			case IDC_BUTTON_PLUS: a += b; break;
+			case IDC_BUTTON_MINUS: a -= b; break;
+			case IDC_BUTTON_ASTER: a *= b; break;
+			case IDC_BUTTON_SLASH: a /= b; break;
+			}
+			input_operation = FALSE;
+
+			if (a == DBL_MIN) strcpy(sz_display, "0");
+			else sprintf(sz_display, "%g", a);
+			SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)sz_display);
 		}
 
 		SetFocus(hwnd);
@@ -241,7 +279,7 @@ INT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		if (wParam >= 0x30 && wParam <= 0x39)
 			SendMessage(hwnd, WM_COMMAND, wParam - 0x30 + IDC_BUTTON_0, 0);
 
-		if(wParam >= VK_NUMPAD0 && wParam <= VK_NUMPAD9)
+		if (wParam >= VK_NUMPAD0 && wParam <= VK_NUMPAD9)
 			SendMessage(hwnd, WM_COMMAND, wParam - VK_NUMPAD0 + IDC_BUTTON_0, 0);
 
 		switch (wParam)
